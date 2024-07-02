e0cdcb25628eb7bb64b1dd168e075728180718ac
Gonca Köprülü <gonca.koprulu@reateknoloji.com>
2024-07-02 20:20:19 +0300

    modal-dialog added

--- react-ecommerce-frontend/src/layouts/MainLayout.jsx
+++ react-ecommerce-frontend/src/layouts/MainLayout.jsx
@@ -1,15 +1,22 @@
-import React from "react";
+import { useState } from "react";
 import Footer from "../components/Layout/Footer/Footer";
 import Header from "../components/Layout/Header/Header";
 import PropTypes from "prop-types";
+import Search from "../components/Modals/Search/Search";
+import Dialog from "../components/Modals/Dialog/Dialog";
 
 const MainLayout = ({ children }) => {
+  const [isSearchShow, setIsSearchShow] = useState(false);
+  const [isDialogShow, setIsDialogShow] = useState(false);
+
   return (
-    <React.Fragment>
-      <Header />
+    <div className="main-layout">
+      <Dialog isDialogShow={isDialogShow} setIsDialogShow={setIsDialogShow} />
+      <Search isSearchShow={isSearchShow} setIsSearchShow={setIsSearchShow} />
+      <Header setIsSearchShow={setIsSearchShow} />
       {children}
       <Footer />
-    </React.Fragment>
+    </div>
   );
 };
 
